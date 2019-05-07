6eb5a8182e7dc6b9048efd0aedfc515f41f75456
Thulasi <atfornet@gmail.com>
2019-05-06 21:50:56 -0700

    Reorganized context.js

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div>
+      <img
+        src="/images/spinner.gif"
+        alt="Loading..."
+        style={{ width: "100px", margin: "auto", display: "block" }}
+      />
+    </div>
+  );
+};
+
+export default Spinner;
